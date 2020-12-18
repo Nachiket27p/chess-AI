ce543b81b08c994a9e1fe79234fc7cd4b7ee3ffe
Nachiket Patel <nachiket27p@gmail.com>
2020-12-18 11:40:39 -0500

    first commit

---
+++ build/CMakeFiles/chess-AI.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess-AI_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/chess-AI_autogen.dir/ParseCache.txt"
+  "chess-AI_autogen"
+  "CMakeFiles/chess-AI.dir/chess-AI_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/chess-AI.dir/main.cpp.o"
+  "chess-AI"
+  "chess-AI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess-AI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
